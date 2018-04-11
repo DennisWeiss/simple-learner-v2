6c01b095c5f2ecd0c8c0d566b4904799c3597fdb
DennisWeiss <weiss.dennis98@yahoo.de>
2018-04-11 22:14:51 +0200

    Add functionality to let a user answer a question

--- simple-learner-v2-backend/src/main/java/com/simplelearner/simplelearner/student/StudentService.java
+++ simple-learner-v2-backend/src/main/java/com/simplelearner/simplelearner/student/StudentService.java
@@ -1,5 +1,7 @@
 package com.simplelearner.simplelearner.student;
 
+import com.simplelearner.simplelearner.answer.Answer;
+import com.simplelearner.simplelearner.answer.AnswerRepository;
 import com.simplelearner.simplelearner.section.Section;
 import com.simplelearner.simplelearner.section.SectionRepository;
 import com.simplelearner.simplelearner.task.Task;
@@ -7,20 +9,25 @@ import com.simplelearner.simplelearner.task.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
 public class StudentService {
     private StudentRepository studentRepository;
     private SectionRepository sectionRepository;
+    private StudentSolvedTaskRepository studentSolvedTaskRepository;
     private TaskRepository taskRepository;
+    private AnswerRepository answerRepository;
 
     @Autowired
-    StudentService(StudentRepository studentRepository, SectionRepository sectionRepository, TaskRepository taskRepository) {
+    StudentService(StudentRepository studentRepository, SectionRepository sectionRepository,
+                   StudentSolvedTaskRepository studentSolvedTaskRepository, TaskRepository taskRepository,
+                   AnswerRepository answerRepository) {
         this.studentRepository = studentRepository;
         this.sectionRepository = sectionRepository;
+        this.studentSolvedTaskRepository = studentSolvedTaskRepository;
         this.taskRepository = taskRepository;
+        this.answerRepository = answerRepository;
     }
 
     void save(Student student) {
@@ -48,19 +55,18 @@ public class StudentService {
         return null;
     }
 
-    Student addTasks(String studentName, List<Long> taskIds) {
+    StudentSolvedTask checkAnswer(String studentName, Long taskId, Long answerId) {
         Optional<Student> studentOptional = studentRepository.findById(studentName);
-        if (studentOptional.isPresent()) {
+        Optional<Task> taskOptional = taskRepository.findById(taskId);
+        Optional<Answer> answerOptional = answerRepository.findById(answerId);
+
+        if (studentOptional.isPresent() && taskOptional.isPresent() && answerOptional.isPresent()) {
             Student student = studentOptional.get();
-            for (Long taskId : taskIds) {
-                Optional<Task> taskOptional = taskRepository.findById(taskId);
-                if (taskOptional.isPresent()) {
-                    Task task = taskOptional.get();
-                    student.addTask(task);
-                }
-            }
-            studentRepository.save(student);
-            return student;
+            Task task = taskOptional.get();
+            Answer answer = answerOptional.get();
+            StudentSolvedTask studentSolvedTask = new StudentSolvedTask(student, task, answer.getIsCorrect());
+            studentSolvedTaskRepository.save(studentSolvedTask);
+            return studentSolvedTask;
         }
         return null;
     }
